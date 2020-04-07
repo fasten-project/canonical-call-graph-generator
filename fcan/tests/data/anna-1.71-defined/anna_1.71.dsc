1345208a8c79448fb23ff22dbe2874241aa74fd3
StefanosChaliasos <stefanoshaliassos@gmail.com>
2020-04-07 16:39:07 +0300

    Update test_utils

---
+++ fcan/tests/data/anna-1.71-defined/anna_1.71.dsc
@@ -0,0 +1,39 @@
+-----BEGIN PGP SIGNED MESSAGE-----
+Hash: SHA512
+
+Format: 3.0 (native)
+Source: anna
+Binary: anna
+Architecture: any
+Version: 1.71
+Maintainer: Debian Install System Team <debian-boot@lists.debian.org>
+Uploaders: Bastian Blank <waldi@debian.org>
+Vcs-Browser: https://salsa.debian.org/installer-team/anna
+Vcs-Git: https://salsa.debian.org/installer-team/anna.git
+Build-Depends: debhelper (>= 9), dpkg-dev (>= 1.15.7), libdebconfclient0-dev (>= 0.46), libdebian-installer4-dev (>= 0.109)
+Package-List:
+ anna udeb debian-installer standard arch=any
+Checksums-Sha1:
+ 6d2c3b1f10b5964c474fa57304d3020ec1a1ddfb 87240 anna_1.75.tar.xz
+Checksums-Sha256:
+ 757c0075c40ea6cb41239a40f9b0b85ee70bf430e5a90f1e9bcf870febc9c12c 87240 anna_1.75.tar.xz
+Files:
+ 766bdbf9e0c814113a4b682433452122 87240 anna_1.75.tar.xz
+
+-----BEGIN PGP SIGNATURE-----
+
+iQJJBAEBCgAzFiEESWrG6BRCSzSFCDUpWfGHyhVusHYFAl5j9loVHGh3YW5zaW5n
+QG1haWxib3gub3JnAAoJEFnxh8oVbrB2gXMP/jVlMqmOI2cAcyVPMPGCS14aLbhN
+h8nY3GaZ75lJlCmmoT7zSuJS0nSyOfJBeY5CykejceR54XAb0Zr4EZsLimFdkT7K
+uzNKi3qyVjbrMcSdXKj3E3Sk4Up568ezlnElRu++oLaNBqYZVbPjyDV0PuqRhtPN
+LC3zskpqGUbC3OVBYANYtTgMNqjTganKFxXYQPz1MN+bPUq5TEs+kFy1cNuf23A8
+mOz/RyLZBoEs2MrlWr1lMHNFhygkYMjBCGWkr7/CtJfgfOcxUjBH6t27H2nF/2eZ
++ub68piXDAVN7ZbSIeXHXUdAJ18wXmPcumWF/gs9d3sRgP92d+CTJAFiOpf+0zum
+yIr5edmv+IJrZLIG9IU6hyRO7S1qAg0jpzDE4WWlHiR8CHES7kgMhqi3rNJDBzax
+BdeJ3yrGQ8qEUCZqsxZcXrea/hwbCx0GCvpun1osBX8kWz3sCDIt3T3rVn84kFHS
+09Tz97DKWifOlmx2/KzIWJqlBNDunV+YnJQUO6IrMH2XNku5swcdsv/SUiJk/4ux
+BDtJ4abSaYsVL21KBX+BGqsoYAy5gMgkrMkVIcxHE3ojc6zmn54rcEPDc0fR8pbM
+oMTI5E4gmzK488sm3qNGpxVk++j5YE7QKuR/A7ulnhNgsBNuTMS/QNNRw9vP7Fro
+J0hEbREpTImh5DUL
+=n9Iy
+-----END PGP SIGNATURE-----
--- fcan/tests/test_utils.py
+++ fcan/tests/test_utils.py
@@ -22,12 +22,14 @@
 # under the License.
 #
 import os
+from shutil import which
 from mock import patch
 from mock import Mock
 from fcan.fcan import safe_split, extract_text, parse_dependency,\
         check_custom_deps, use_mvn_spec, parse_changelog,\
         convert_debian_time_to_unix, canonicalize_path, run_command,\
-        find_shared_libs_util
+        parse_dsc_file, parse_deb_file, canonicalize_binary_name,\
+        find_product, find_shared_libs_products, find_static_libraries_products
 
 
 def get_directory(filename):
@@ -37,18 +39,64 @@ def get_directory(filename):
     return directory
 
 
-class find_product_mock(Mock):
+def check_command_exist(cmd):
+    return which(cmd) is not None
+
+
+def read_return(path, parse_stdout):
+    with open(path, 'r') as f:
+        res = f.read()
+        res = res.encode("utf-8")
+        if parse_stdout:
+            return res.decode("utf-8").split("\n"), 0
+        return res, 0
+
+
+class run_command_mock(Mock):
     def __call__(self, *args, **kwargs):
-        path = args[0]
-        if path == 'libdebconfclient.so.0':
-            return 'libdebconfclient0', 0
-        elif path == 'libdebian-installer.so.4':
-            return 'libdebian-installer4', 0
-        elif path == 'libc.so.6':
-            return 'libc6', 0
-        elif path == '/lib64/ld-linux-x86-64.so.2':
-            return 'libc6', 0
-        return '', 1
+        data = get_directory('mocked_data')
+        arguments = args[0]
+        parse_stdout = True
+        if len(args) == 2:
+            parse_stdout = args[1]
+        if arguments[0] == 'dpkg':
+            if arguments[1] == '-S':
+                base = '{}/dpkg/S/'.format(data)
+                if arguments[2] == '/lib/x86_64-linux-gnu/libc.so.6':
+                    filepath = base + 'x86_libc.so.6'
+                    return read_return(filepath, parse_stdout)
+                elif arguments[2] == 'libc.so.6':
+                    filepath = base + 'libc.so.6'
+                    return read_return(filepath, parse_stdout)
+                elif arguments[2] == 'libdebconfclient.so.0':
+                    filepath = base + 'libdebconfclient.so.0'
+                    return read_return(filepath, parse_stdout)
+                elif arguments[2] == 'libdebian-installer.so.4':
+                    filepath = base + 'libdebian-installer.so.4'
+                    return read_return(filepath, parse_stdout)
+                elif arguments[2] == '/lib64/ld-linux-x86-64.so.2':
+                    filepath = base + 'ld-linux-x86-64.so.2'
+                    return read_return(filepath, parse_stdout)
+                elif arguments[2] == '/usr/lib/x86_64-linux-gnu/libdebian-installer.a':
+                    filepath = base + 'libdebian-installer.a'
+                    return read_return(filepath, parse_stdout)
+                elif arguments[2] == '/usr/lib/gcc/x86_64-linux-gnu/9/libgcc.a':
+                    filepath = base + 'libgcc.a'
+                    return read_return(filepath, parse_stdout)
+                elif arguments[2] == '/usr/lib/x86_64-linux-gnu/libc.a':
+                    filepath = base + 'libc.a'
+                    return read_return(filepath, parse_stdout)
+            elif arguments[1] == '-I':
+                base = '{}/dpkg/I/'.format(data)
+                if arguments[2].endswith('mydeb.udeb'):
+                    filepath = base + 'mydeb.udeb'
+                    return read_return(filepath, parse_stdout)
+        elif arguments[0] == 'ldd':
+            if arguments[1] == '-d':
+                base = '{}/ldd/d/'.format(data)
+                if arguments[2] == 'anna':
+                    filepath = base + 'anna'
+                    return read_return(filepath, parse_stdout)
 
 
 def test_safe_split():
@@ -81,27 +129,57 @@ def test_extract_text():
         "Should return ('', 'Hello world')"
 
 
-simple_product = {'architectures': "", 'constraints': "[9,)",
-                  'forge': "debian", 'product': "debhelper"}
+simple_product = {'architectures': [], 'constraints': "[9,)",
+                  'forge': "debian", 'product': "debhelper",
+                  'dependency_type': "Depends", 'is_virtual': False,
+                  'alternatives': []}
 complex_product = {
-        'architectures': "",
+        'architectures': ["amd64"],
         'constraints': "",
         'forge': "debian",
-        'product': "libdebconfclient-dev",
+        'product': "libdebian-installer4-dev",
+        'is_virtual': False,
+        'dependency_type': "Depends",
         'alternatives': [{
-            'architectures': "amd64",
+            'architectures': [],
             'constraints': "",
+            'dependency_type': "Depends",
+            'is_virtual': False,
             'forge': "debian",
-            'product': "libdebian-installer4-dev"
+            'product': "libdebconfclient-dev",
+            'alternatives': []
          }]
 }
 
 
 def test_parse_dependency():
-    assert parse_dependency("debhelper (>= 9)", "debian") == simple_product
+    assert parse_dependency("debhelper (>= 9)", "debian", "Depends") ==\
+            simple_product
     assert parse_dependency(
         "libdebian-installer4-dev [amd64] | libdebconfclient-dev",
-        "debian") == complex_product
+        "debian", "Depends") == complex_product
+
+
+def test_run_command():
+    assert run_command(['echo', 'hello'], False)[0] == b'hello\n'
+    assert run_command(['echo', 'hello'])[0] == ['hello', '']
+    assert run_command(['echo', 'hello'])[1] == 0
+    assert run_command(['echo111', 'hello'])[0] == ''
+    assert run_command(['echo111', 'hello'])[1] == -1
+
+
+@patch("fcan.fcan.run_command", new_callable=run_command_mock)
+def test_find_product(mock_run_command):
+    paths = [
+        '/lib/x86_64-linux-gnu/libc.so.6',
+        'libc.so.6'
+    ]
+    results = [
+        ('libc6', 0),
+        ('libc6', 0)
+    ]
+    for p, r in zip(paths, results):
+        assert find_product(p) == r
 
 
 def test_check_custom_deps():
@@ -139,6 +217,36 @@ def test_parse_changelog():
             'Mon, 13 Feb 2017 07:08:47 +0100')
 
 
+anna_dsc_res = {
+    'Build-Depends': "debhelper (>= 9), dpkg-dev (>= 1.15.7), libdebconfclient0-dev (>= 0.46), libdebian-installer4-dev (>= 0.109)"
+}
+
+
+def test_parse_dsc_file():
+    anna_dsc = get_directory('anna-1.71-defined') + '/anna_1.71.dsc'
+    assert parse_dsc_file(anna_dsc) == anna_dsc_res
+
+
+anna_deb_res = {
+    'Package': "anna",
+    'Version': "1.71",
+    'Architecture': "amd64",
+    'Depends': "libc6-udeb (>= 2.28), libdebconfclient0-udeb, libdebian-installer4-udeb (>= 0.119), cdebconf-udeb"
+}
+
+
+@patch("fcan.fcan.run_command", new_callable=run_command_mock)
+def test_parse_deb_file_mock_run_command(mock_run_command):
+    anna_deb = get_directory('anna-1.71-defined') + '/mydeb.udeb'
+    assert parse_deb_file(anna_deb) == anna_deb_res
+
+
+def test_parse_deb_file():
+    if check_command_exist('dpkg'):
+        anna_deb = get_directory('anna-1.71-defined') + '/mydeb.udeb'
+        assert parse_deb_file(anna_deb) == anna_deb_res
+
+
 def test_convert_debian_time_to_unix():
     d1 = "Sun, 05 Mar 2017 12:26:20 +0100"
     epoch1 = "1488709580"
@@ -150,6 +258,7 @@ def test_canonicalize_path():
         './libmisc/walk_tree.c',
         '/usr/include/cdebconf/debconfclient.h',
         '/build/anna-oYTzHL/anna-1.71/anna.c',
+        '/build/anna-oYTzHL/anna-1.71/src/anna.c',
         '/build/mlocate-OLrxYu/mlocate-0.26/build/../src/conf.c',
         '/build/mlocate-OLrxYu/mlocate-0.26/build/gnulib/lib/../../../gnulib/lib/mbuiter.h',
         './build/gnulib/lib/../../../gnulib/lib/mbuiter.h',
@@ -159,6 +268,7 @@ def test_canonicalize_path():
         'libmisc/walk_tree.c',
         '/usr/include/cdebconf/debconfclient.h',
         'anna.c',
+        'src/anna.c',
         'src/conf.c',
         'gnulib/lib/mbuiter.h',
         'gnulib/lib/mbuiter.h',
@@ -168,19 +278,50 @@ def test_canonicalize_path():
         assert canonicalize_path(p) == r
 
 
-def test_run_command():
-    assert run_command(['echo', 'hello'], False)[0] == b'hello\n'
-    assert run_command(['echo', 'hello'])[0] == ['hello', '']
-    assert run_command(['echo', 'hello'])[1] == 0
-    assert run_command(['echo111', 'hello'])[0] == ''
-    assert run_command(['echo111', 'hello'])[1] == -1
-
-
-def test_find_shared_libs_util():
-    with open('tests/data/cmds/lddout', 'r') as f:
-        stdout = f.readlines()
-    res = ['/lib/x86_64-linux-gnu/libdebconfclient.so.0',
-           '/lib/x86_64-linux-gnu/libdebian-installer.so.4',
-           '/lib/x86_64-linux-gnu/libc.so.6', '/lib64/ld-linux-x86-64.so.2'
+def test_canonicalize_binary_name():
+    binaries = [
+        '/lib/x86_64-linux-gnu/libc.so.6',
+        '/lib/x86_64-linux-gnu/libc.a',
+        '/lib/x86_64-linux-gnu/libc.so',
+        'libc.so',
+        'libc.a',
+        'libc.a.00',
+        'libc.so.0.0.1',
+        'libc.so.0.0.0a'
     ]
-    assert find_shared_libs_util(stdout) == res
+    results = [
+        'libc.so',
+        'libc.a',
+        'libc.so',
+        'libc.so',
+        'libc.a',
+        'libc.a',
+        'libc.so',
+        'libc.so'
+    ]
+    for b, r in zip(binaries, results):
+        assert canonicalize_binary_name(b) == r
+
+
+@patch("fcan.fcan.run_command", new_callable=run_command_mock)
+def test_find_shared_libs_products(mock_run_command):
+    res = [
+        ('/lib/x86_64-linux-gnu/libdebconfclient.so.0', 'libdebconfclient0'),
+        ('/lib/x86_64-linux-gnu/libdebian-installer.so.4', 'libdebian-installer4'),
+        ('/lib/x86_64-linux-gnu/libc.so.6', 'libc6'),
+        ('/lib64/ld-linux-x86-64.so.2', 'libc6')
+    ]
+    assert set(find_shared_libs_products('anna')) == set(res)
+
+
+@patch("fcan.fcan.run_command", new_callable=run_command_mock)
+def test_find_static_libraries_products(mock_run_command):
+    cs = get_directory('anna-1.71-defined') + '/anna.cs'
+    reg = '^/build/anna/anna-1.71.*'
+    product = 'anna'
+    res = [
+        ('/usr/lib/x86_64-linux-gnu/libc.a', 'libc6-dev'),
+        ('/usr/lib/x86_64-linux-gnu/libdebian-installer.a', 'libdebian-installer4-dev'),
+        ('/usr/lib/gcc/x86_64-linux-gnu/9/libgcc.a', 'libgcc-9-dev')
+    ]
+    assert set(find_static_libraries_products(cs, product, reg)) == set(res)
